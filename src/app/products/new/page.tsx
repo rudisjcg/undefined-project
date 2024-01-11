c917bce1cc75c72e170ecf5f6ac1fe8856ea7a88
Rudis <82242977+rudisjcg@users.noreply.github.com>
2024-01-11 02:19:58 -0400

    error
    Error with a { }

--- src/app/products/new/page.tsx
+++ src/app/products/new/page.tsx
@@ -146,23 +146,25 @@ export default function NewProduct() {
             </Layout>
         );
     }
+}
 
-    function ArrowUpRightIcon(props: any) {
-        return (
-            <svg
-                {...props}
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            >
-                <path d="M7 7h10v10" />
-                <path d="M7 17 17 7" />
-            </svg>
-        );
-    }
+function ArrowUpRightIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M7 7h10v10" />
+            <path d="M7 17 17 7" />
+        </svg>
+    );
+}
+

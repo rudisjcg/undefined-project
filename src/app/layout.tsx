cc78d681ae83248ebc82e3cefc1b4eee32f3fd0d
krakatua <rudisjcg@gmail.com>
2024-01-24 17:36:11 -0400

    Layout fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
-import type { GetServerSideProps, Metadata } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./context/client-provider";
 import { getServerSession } from "next-auth";
+import authOptions from "./api/auth/[...nextauth]/option";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req);
-
-  return {
-    props: {
-      session,
-    },
-  };
-};
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +14,10 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  session,
 }: {
-  session: any;
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
       <Provider session={session}>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useSession } from "next-auth/react";
 import Nav from "./Nav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { data: session } = useSession();
   return (
     <>
       <div className="flex">

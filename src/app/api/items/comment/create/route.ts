3108ba9673a7a4c4574faa4152bb921d3dc8c4b9
krakatua <rudisjcg@gmail.com>
2024-02-14 10:03:55 -0400

    Comments routes and logic done

--- src/app/api/items/comment/create/route.ts
+++ src/app/api/items/comment/create/route.ts
@@ -2,7 +2,7 @@ import authOptions from "@/app/api/auth/[...nextauth]/option";
 import { mongooseConnect } from "@/lib/mongoose";
 import Comment from "@/models/comment";
 import Item from "@/models/items";
-import User from "@/models/user";
+import { ratingItemByComments } from "@/utils";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
@@ -12,14 +12,35 @@ export async function POST(req: Request, res: NextResponse) {
     await mongooseConnect();
     const data = await getServerSession(authOptions);
     const { comment, rating, itemId, images } = await req.json();
-    const findItem = await Item.findById(itemId);
 
-    // if (data === null) {
-    //     return NextResponse.json({ message: "You are not logged in" });
-    // }
+    if (!data) {
+        return NextResponse.json({ message: "You are not logged in" });
+    }
 
-    console.log(comment, rating, itemId, images)
-    console.log(findItem)
+    try {
+        const commentToSave = await Comment.create({
+            comment,
+            rating,
+            itemId,
+            images,
+        });
+
+
+
+
+        const itemFoundToAddComment = await Item.findOneAndUpdate({
+            _id: itemId
+        }, {
+            $push: {
+                comments: commentToSave._id
+            }
+        });
+
+        console.log(itemFoundToAddComment._doc)
+        return NextResponse.json({ message: "Comment created", success: true });
+    } catch {
+        return NextResponse.json({ message: "You are not logged in", success: false });
+    }
 
 
 
